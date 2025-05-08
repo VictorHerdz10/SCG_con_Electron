fbd241d06dd898cf3d3d51e8db812dadfe94d985
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-05-08 04:57:44 -0400

    fix all

--- backend/routes/registrosContratosRoutes.js
+++ backend/routes/registrosContratosRoutes.js
@@ -14,18 +14,14 @@ import {
   actualizarSuplemento,
   eliminarSuplemento,
   useSupplement,
-  obtenerEspecificos,
+  obtenerEspecificos
 } from "../controllers/registrosContratosController.js";
 import upload from "../middleware/uploadPdf.js";
 const router = express.Router();
 
 //Ruta Privada
 router.post("/", checkAuth, upload.single("subirPDF"), registrarContrato);
-router.get(
-  "/listar-registro-contratos/:tipoContrato",
-  checkAuth,
-  obtenerRegistroContratos
-);
+router.get("/listar-registro-contratos/:tipoContrato", checkAuth, obtenerRegistroContratos);
 router.put(
   "/actualizar-registro-contrato/:id",
   checkAuth,
@@ -37,20 +33,25 @@ router.delete(
   checkAuth,
   eliminarRegistroContrato
 );
-router.post("/filtrar-contratos", checkAuth, obtenerContratosFiltrados);
+router.post(
+  "/filtrar-contratos",
+  checkAuth,
+  obtenerContratosFiltrados
+);
 
-router.get("/notificacion-contratos", checkAuth, notificarcontratos);
-router.get("/marcar-leida/:id", checkAuth, marcarComoLeidas);
-router.get("/marcar-leidas-all", checkAuth, marcarleidasAll);
+router.get("/notificacion-contratos",checkAuth,notificarcontratos);
+router.get("/marcar-leida/:id",checkAuth,marcarComoLeidas);
+router.get('/marcar-leidas-all',checkAuth,marcarleidasAll);
 
 //Suplementos
-router.get("/suplementos/:id", checkAuth, getSuplementosByContrato);
-router.get("/suplementos/usar/:id", checkAuth, useSupplement);
-router.post("/suplementos/:id", checkAuth, crearSuplemento);
-router.put("/suplementos/:id", checkAuth, actualizarSuplemento);
-router.delete("/suplementos/:id", checkAuth, eliminarSuplemento);
+router.get('/suplementos/:id',checkAuth,getSuplementosByContrato);
+router.get('/suplementos/usar/:id',checkAuth,useSupplement);
+router.post('/suplementos/:id',checkAuth, crearSuplemento);
+router.put('/suplementos/:id',checkAuth, actualizarSuplemento);
+router.delete('/suplementos/:id',checkAuth, eliminarSuplemento);
 
 // routes/contratos.js
-router.get("/marco/:id/especificos", checkAuth, obtenerEspecificos);
+router.get('/marco/:id/especificos',checkAuth, obtenerEspecificos);
+
 
 export default router;
